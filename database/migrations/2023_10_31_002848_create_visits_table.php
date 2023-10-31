f8ba65300e9827cd18b9a67bf4df0d6f8c181db5
ali3amer <42501649+ali3amer@users.noreply.github.com>
2023-10-30 17:38:57 -0700

    add visits

---
+++ database/migrations/2023_10_31_002848_create_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->unsignedBigInteger('insurance_id')->nullable();
+            $table->foreign('insurance_id')->references('id')->on('insurances');
+            $table->decimal("discount", 10, 8);
+            $table->decimal("total_amount", 10, 8);
+            $table->string("comment")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
